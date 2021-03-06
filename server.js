3840f23e01c6d3f14b26e0065e649232b9494f65
krishnendu-halder <krishnendu.halder@moolya.com>
2021-03-06 11:22:37 +0530

    Add Procfile and Modify server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
 })
